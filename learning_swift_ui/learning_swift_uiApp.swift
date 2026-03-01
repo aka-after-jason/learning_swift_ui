d5342dcf6ff378b6b56ca035f6000a7c1b230fff
jason <15021453094@163.com>
2026-03-02 06:50:06 +0800

    add .gitignore

---
+++ learning_swift_ui/learning_swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_swift_uiApp.swift
+//  learning_swift_ui
+//
+//  Created by Elaine on 2026/3/2.
+//
+
+import SwiftUI
+
+@main
+struct learning_swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
