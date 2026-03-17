fbaf9f45661746518b260437d6e4f16e91d7bcb7
jason <15021453094@163.com>
2026-03-17 14:00:22 +0800

    功能: 添加 KeychainSwift 库, 还没有练习: TODO

---
+++ learning_swift_ui/Bootcamp/KeychainBootcamp.swift
@@ -0,0 +1,24 @@
+//
+//  KeychainBootcamp.swift
+//  learning_swift_ui
+//
+//  Created by Elaine on 2026/3/17.
+//
+
+import SwiftUI
+// https://github.com/evgenyneu/keychain-swift
+
+// Keychain is similar to UserDefaults or AppStorage
+// Keychain is encrypted
+// Keychain is good for: password, ssn, credit card info, sensitive data
+// keychain persists between installs and across devices
+
+struct KeychainBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KeychainBootcamp()
+}
